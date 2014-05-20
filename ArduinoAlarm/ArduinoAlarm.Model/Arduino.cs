9c53cd7d322ec1b0c228822dce890d8ac3dc4046
Odonno <bottiau.david@laposte.net>
2014-05-20 11:10:21 +0200

    Add methods to witch on LEDs

---
+++ ArduinoAlarm/ArduinoAlarm.Model/Arduino.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Ports;
+
+namespace ArduinoAlarm.Model
+{
+    public class Arduino
+    {
+        #region Properties
+
+        public SerialPort SerialPort { get; private set; }
+        public const string COMport = "COM5";
+        public const int BaudRate = 9600;
+
+        #endregion
+
+
+        #region Contructor
+
+        public Arduino()
+        {
+            SerialPort = new SerialPort(COMport, BaudRate);
+        }
+
+        #endregion
+    }
+}
--- ArduinoAlarm/ArduinoAlarm.View/MainWindow.xaml.cs
+++ ArduinoAlarm/ArduinoAlarm.View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ArduinoAlarm.Model;
 
 namespace ArduinoAlarm.View
 {
@@ -20,9 +21,34 @@ namespace ArduinoAlarm.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Arduino _arduino = new Arduino();
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void btAllumerLed_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchLeds(true);
+        }
+
+        private void btEteindreLed_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchLeds();
+        }
+
+        private void SwitchLeds(bool on = false)
+        {
+            _arduino.SerialPort.Open();
+            byte onByte = on ? (byte)1 : (byte)0;
+
+            for (byte i = 5; i < 8; i++)
+                _arduino.SerialPort.Write(new [] { i, onByte }, 0, 2);
+
+            _arduino.SerialPort.Close();
         }
     }
 }
